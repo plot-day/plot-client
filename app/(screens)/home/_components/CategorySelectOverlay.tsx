2bf014bf96e5477d06182daa0c42cf55ac39c637
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-10-23 18:34:59 -0700

    Feat: CategorySelectOverlay | Implement UI #7

---
+++ app/(screens)/home/_components/CategorySelectOverlay.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Button from '@/components/button/Button';
+import IconHolder from '@/components/icon/IconHolder';
+import Overlay from '@/components/overlay/Overlay';
+import { categoriesAtom, CategoryType } from '@/store/category';
+import { groupsAtom } from '@/store/group';
+import { useAtomValue } from 'jotai';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { FaPencil, FaPlus } from 'react-icons/fa6';
+
+const CategorySelectOverlay = () => {
+  const { data: groups } = useAtomValue(groupsAtom);
+  const { data: categories } = useAtomValue(categoriesAtom);
+
+  return (
+    <Overlay id="category-select" title="Select category" isRight={true}>
+      {groups?.map(({ id, title }) => {
+        const filteredCategories = categories?.filter(
+          (category) => category.groupId === id
+        );
+        return (
+          <ul className="mb-8 space-y-2">
+            {!!filteredCategories?.length && (
+              <h6 key={id} className="text-sm font-extrabold">
+                {title}
+              </h6>
+            )}
+            {filteredCategories?.map((category) => (
+              <CategorySelectItem {...category} />
+            ))}
+          </ul>
+        );
+      })}
+    </Overlay>
+  );
+};
+
+const CategorySelectItem = ({ id, title, icon, group }: Partial<CategoryType>) => {
+  const router = useRouter();
+
+  const selectCategoryHandler = () => {
+    router.replace(`?log-input=show&categoryId=${id}`);
+  };
+
+  return (
+    <li key={title} className="w-full flex items-center justify-between cursor-pointer">
+      <div className="w-full flex gap-2 items-center" onClick={selectCategoryHandler}>
+        <IconHolder isCircle={true}>{icon}</IconHolder>
+        <div className="text-left w-full">
+          <p className="font-bold leading-tight">{title}</p>
+        </div>
+      </div>
+      {/* <Button className="px-2 py-1 text-xs rounded-md shrink-0" onClick={addLogHandler}>
+        Add Log
+      </Button> */}
+    </li>
+  );
+};
+
+export default CategorySelectOverlay;
