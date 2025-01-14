a9ea6cd1a69cc080b85706df59835f94ef40303e
lynj <82319291+earlyoonj@users.noreply.github.com>
2025-01-14 10:57:48 -0800

    Feat: API | Implement CRUD #11

---
+++ app/api/log/route.ts
@@ -0,0 +1,57 @@
+import { authOptions } from '@/lib/auth';
+import { prisma } from '@/prisma/client';
+import { getServerSession } from 'next-auth';
+import { NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  const searchParams = req.nextUrl.searchParams;
+  const dateParam = searchParams.get('date');
+  const date = dateParam ? new Date(dateParam) : undefined;
+
+  try {
+    const data = await prisma.log.findMany({
+      where: { userId: session.user.id, date },
+      include: { category: true },
+      orderBy: [{ createdAt: 'asc' }],
+    });
+
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to fetch logs', { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  const reqData = await req.json();
+  try {
+    const data = await prisma.log.create({
+      data: {
+        ...reqData,
+        userId: session.user.id,
+      },
+      include: { goal: true }
+    });
+
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to create log', { status: 500 });
+  }
+}
